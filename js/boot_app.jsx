956f1698a3e3bd766017b412134e8e1b8ae4289c
Pete Hodgson <git@thepete.net>
2015-01-03 23:13:24 -0800

    convert boot_app.js to .jsx

--- js/boot_app.jsx
+++ js/boot_app.jsx
@@ -2,10 +2,9 @@ var React = require('react');
 var MainView = require('./views/main_view');
 
 var bootApp = function(appContainer){
-  return React.render(
-    React.createElement(MainView),
-    appContainer
-  );
+  var theApp = <MainView/>;
+
+  return React.render( theApp, appContainer);
 }
 
 module.exports = bootApp;
