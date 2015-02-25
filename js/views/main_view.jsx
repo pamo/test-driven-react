a48b992773e21bf5e5bec1de3401a71f81b3c8c2
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-02-25 08:21:37 +0100

    Revert "Added a build list and build view"
    This reverts commit 30c9a387a3f55394fe57bd3261749bdcc1f401cd.

--- js/views/main_view.jsx
+++ js/views/main_view.jsx
@@ -1,14 +1,8 @@
 var React = require('react'),
-    _ = require('underscore'),
-    BuildList = require('./build_list_view');
+    _ = require('underscore');
+
 
 module.exports = React.createClass({
   render: function(){
-    return (
-      <div className="mainView">
-      <h1>Builds</h1>
-      <BuildList />
-    </div>
-    );
   }
 });
